86ce8ff220fe5f418342f5ada63db9558cd58951
MikesMikes <michaelkaliber15@gmail.com>
2022-01-31 14:32:30 +0000

    Working branch at di-in-java-config

--- src/main/java/guru/springframework/sfgdi/repositories/EnglishGreetingRepositoryImpl.java
+++ src/main/java/guru/springframework/sfgdi/repositories/EnglishGreetingRepositoryImpl.java
@@ -6,6 +6,6 @@ package guru.springframework.sfgdi.repositories;
 public class EnglishGreetingRepositoryImpl implements EnglishGreetingRepository {
     @Override
     public String getGreeting() {
-        return "Hello World - EN";
+        return "Hello World - EN - From EnglishGreetingRepositoryImpl";
     }
 }
